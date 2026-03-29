7efbd470d3d2ce90222547aab3dee610291f42c9
ZHANG DAWEI <kalijerryuk@gmail.com>
2026-03-29 20:21:42 +0100

    refactor: clean up components index exports

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,31 +1,23 @@
-import { Logo } from './brand';
-import { CommandPaletteButton, ModeIndicator, ProductModeSelector, ThemeToggle } from './controls';
-import { MainContent, RightPanel, Sidebar } from './layout';
-import {
-  ActionCard,
-  Button,
-  InfoRow,
-  Input,
-  Modal,
-  RadioButton,
-  SectionLabel,
-  StatCard,
-  StatusDot,
-  StatusPill,
-  Surface,
-} from './design-system';
-
 // Modals
 export { AddGroupModal } from './AddGroupModal';
 export { AddServerModal } from './AddServerModal';
 export { EditGroupModal } from './EditGroupModal';
 export { EditServerModal } from './EditServerModal';
 
-// Components
+// Core Components
 export { Header } from './Header';
 export { ServerItem, ServerList } from './ServerList';
 export { SplitScreen } from './SplitScreen';
 export { Terminal } from './Terminal';
+
+// Brand Components
+export { Logo } from './brand';
+
+// Control Components
+export { CommandPaletteButton, ModeIndicator, ProductModeSelector, ThemeToggle } from './controls';
+
+// Layout Components
+export { MainContent, RightPanel, Sidebar } from './layout';
 
 // Design System
 export {
@@ -40,16 +32,7 @@ export {
   StatusDot,
   StatusPill,
   Surface,
-};
-
-// Brand
-export { Logo };
-
-// Controls
-export { CommandPaletteButton, ModeIndicator, ProductModeSelector, ThemeToggle };
-
-// Layout
-export { MainContent, RightPanel, Sidebar };
+} from './design-system';
 
 // Type exports
 export type { ButtonSize, ButtonVariant } from './design-system';
