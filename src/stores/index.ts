352da8483cad3a18e77fd5e3356e722d5cade748
ZHANG DAWEI <kalijerryuk@gmail.com>
2026-03-29 20:24:47 +0100

    refactor: finalize stores index organization

--- src/stores/index.ts
+++ src/stores/index.ts
@@ -1,5 +1,24 @@
-export { addCommandToHistory, getCommandHistory, getGlobalCommandHistory, useCommandHistoryStore } from './commandHistoryStore';
+// Domain store exports
+export {
+  addCommandToHistory,
+  getCommandHistory,
+  getGlobalCommandHistory,
+  useCommandHistoryStore,
+} from './commandHistoryStore';
+
 export { useServerStore } from './serverStore';
+
 export { useSessionStore, type TerminalSession } from './sessionStore';
-export { useTeamStore, type AuditLog, type Team } from './teamStore';
+
+export { useTeamStore } from './teamStore';
+
 export { useUiStore, type UiState } from './uiStore';
+
+// Domain types
+export * from './domain';
+
+// Hooks
+export * from './hooks';
+
+// Utils
+export * from './utils';
